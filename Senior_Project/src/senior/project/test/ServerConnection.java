5c5cb311837a5b9a8db83b71a8395c7ce5d15f28
Paul Jones <shirlypunk@hotmail.com>
2011-11-21 11:17:20 -0500

    Start on server connectivity module The ServerConnection interface is for specifying the methods that a connection to the server must implement. ServerSession is the class that implements that interface.

---
+++ Senior_Project/src/senior/project/test/ServerConnection.java
@@ -0,0 +1,10 @@
+package senior.project.test;
+
+/**
+ * The class that connects to the server will implement this interface, any
+ * required server functions should be specified in this interface
+ * @author Paul Jones
+ */
+public interface ServerConnection {
+  
+}
---
+++ Senior_Project/src/senior/project/test/ServerSession.java
@@ -0,0 +1,9 @@
+package senior.project.test;
+
+/**
+ *
+ * @author Paul Jones
+ */
+public class ServerSession {
+  
+}
